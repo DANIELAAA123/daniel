4fb9de7b01ab2cb7a7216fabb126a5d9b0770ab6
Daniel Aktan <2168809@talnet.nl>
2023-05-08 10:05:54 +0100

    first commit

---
+++ php/test.php
@@ -0,0 +1,23 @@
+<?php 
+$getal=12;
+$getal1=10;
+echo $getal + $getal1;
+echo "<br>";
+$getal2=12;
+$getal3=10;
+echo $getal2 * $getal3;
+echo "<br>";
+$naam="Daniel";
+echo "welkom ".$naam;
+
+
+
+// deze code bevat veel fouten
+//  haal alle fouten eruit!
+
+
+$mijnNaam="Carl";
+$jouwNaam="Camel";
+echo $mijnNaam. " zit bij " .$jouwNaam. " in de klas!";
+
+?>
